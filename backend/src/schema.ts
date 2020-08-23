7213a24e73976602a6b0af2eec476251080371b8
TheoMer <6e240757@use.startmail.com>
2020-08-23 13:26:19 +0100

    Update schema.ts

--- backend/src/schema.ts
+++ backend/src/schema.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import * as types from './types';
 import { join } from 'path'
+import { Context } from './types2'
 
 import { makeSchema } from '@nexus/schema';
 import { nexusSchemaPrisma } from 'nexus-plugin-prisma/schema';
@@ -8,28 +9,27 @@ import { nexusSchemaPrisma } from 'nexus-plugin-prisma/schema';
 const nexusPrisma = nexusSchemaPrisma({
     experimentalCRUD: true,
     paginationStrategy: 'prisma',
-    //prismaClient: (ctx: Context) => ctx.prisma,
+    prismaClient: (ctx: Context) => ctx.prisma,
   })
 
 export const schema = makeSchema({
   types,
-  //plugins: [nexusSchemaPrisma()],
   plugins: [nexusPrisma],
   outputs: {
     schema: path.join(__dirname, './../schema.graphql'),
     typegen: path.join(__dirname, './generated/nexus.ts'),
   },
-  /*typegenAutoConfig: {
+  typegenAutoConfig: {
     sources: [
       {
         source: '@prisma/client',
         alias: 'prisma',
       },
       {
-        source: join(__dirname, 'types.ts'),
+        source: join(__dirname, 'types2.ts'),
         alias: 'ctx',
       },
     ],
     contextType: 'ctx.Context',
-  },*/
+  },
 });
